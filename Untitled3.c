12bfaa57068456e71965c9a2a38bdb618c7982e4
Minhtayto <nguyenphamcongminh07@gmail.com>
2024-02-21 00:17:35 +0700

    tap1

---
+++ Untitled3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+   int age = 18;
+   int days = 21;
+   int months = 07;
+   int years = 2005;
+   scanf("%d%d%d%d", &age, &days, &months, &years);
+   printf("Tuoi: %d\n", age);
+   printf("ngay: %d\n", days);
+   printf("Thang: %d\n", months);
+   printf("Namsinh: %d", years);
+   
+   return 0;
+}
+   
